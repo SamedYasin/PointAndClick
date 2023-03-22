3f3dd3a58eecdcc35bd4364dca7ab8c67e80d9fc
SamedYasin <127936936+SamedYasin@users.noreply.github.com>
2023-03-22 10:55:39 +0100

    jatsoch

--- js/Index.js
+++ js/Index.js
@@ -1,2 +1,17 @@
 document.getElementById("mainTitle").appendChild(document.createTextNode("Point And Click Adventure"));
 document.getElementById("mainTitle").style.textAlign = "center";
+
+const offsetCharacter = 16;
+
+const mainCharacter = document.getElementById("mainCharacter");
+const gameWindow = document.getElementById("gameWindow");
+
+gameWindow.onClick = function (e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    mainCharacter.style.left - offsetCharacter +"px";
+    mainCharacter.style.top - offsetCharacter +"px";
+    Console.log(x);
+    
+};
